0b3f3a2f6e932152dbb83b924e5e1ef651d46d4a
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-03-15 22:15:49 +0000

    100 done need to add styling

--- script.js
+++ script.js
@@ -6,7 +6,31 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  for (let episode of episodeList) {
+    displayEpisode(episode);
+  }
 }
 
 window.onload = setup;
+
+
+function displayEpisode(episode) {
+  const rootEl = document.querySelector('#root');
+  const seasonNr = String(episode.season).padStart(2, 0);
+  const episodeNr = String(episode.number).padStart(2, 0);
+  const cardEl = document.createElement('div');
+  cardEl.className = 'card-info';
+  const epTitleEl = document.createElement('h1');
+  epTitleEl.className = 'card-title';
+  epTitleEl.innerText = `${episode.name} S${seasonNr}E${episodeNr}`;
+  cardEl.appendChild(epTitleEl);
+  const pictureEl = document.createElement('img');
+  pictureEl.src = episode.image.medium;
+  pictureEl.className = 'card-img';
+  cardEl.appendChild(pictureEl);
+  const summaryEl = document.createElement('div');
+  summaryEl.innerHTML = episode.summary;
+  summaryEl.className = 'card-summary';
+  cardEl.appendChild(summaryEl);
+  rootEl.appendChild(cardEl);
+}
