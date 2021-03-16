a6c0fa0ee2586cda51c7d8d249b9f2a148a2ffc7
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-03-16 17:30:31 +0000

    200 done

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
+let allEpisodes;
+const rootEl = document.querySelector('#root');
+const searchInput = document.querySelector('#search-input');
+const searchResult = document.querySelector('#search-result');
+searchInput.innerText = '';
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  for (let episode of episodeList) {
-    displayEpisode(episode);
-  }
+searchInput.addEventListener('keyup', (input) => {
+  const searchEl = input.target.value.toLowerCase();
+  rootEl.innerHTML = '';
+  const filteredEpisodes = allEpisodes.filter(episode => {
+    return (episode.name.toLowerCase().includes(searchEl) || episode.summary.toLowerCase().includes(searchEl));
+  });
+  searchResult.innerHTML = `Displaying ${filteredEpisodes.length}/${allEpisodes.length}`;
+  displayEpisode(filteredEpisodes);
+});
+
+function setup() {
+  allEpisodes = getAllEpisodes();
+  displayEpisode(allEpisodes);
+  displayEpisode(filteredEpisodes);
 }
 
 window.onload = setup;
 
-
-function displayEpisode(episode) {
-  const rootEl = document.querySelector('#root');
-  const seasonNr = String(episode.season).padStart(2, 0);
-  const episodeNr = String(episode.number).padStart(2, 0);
-  const cardEl = document.createElement('div');
-  cardEl.className = 'card-info';
-  const epTitleEl = document.createElement('h1');
-  epTitleEl.className = 'card-title';
-  epTitleEl.innerText = `${episode.name} S${seasonNr}E${episodeNr}`;
-  cardEl.appendChild(epTitleEl);
-  const pictureEl = document.createElement('img');
-  pictureEl.src = episode.image.medium;
-  pictureEl.className = 'card-img';
-  cardEl.appendChild(pictureEl);
-  const summaryEl = document.createElement('div');
-  summaryEl.innerHTML = episode.summary;
-  summaryEl.className = 'card-summary';
-  cardEl.appendChild(summaryEl);
-  rootEl.appendChild(cardEl);
+function displayEpisode(episodeList) {
+  for (let episode of episodeList) {
+    const seasonNr = String(episode.season).padStart(2, 0);
+    const episodeNr = String(episode.number).padStart(2, 0);
+    const cardEl = document.createElement('div');
+    cardEl.className = 'card-info';
+    const epTitleEl = document.createElement('h1');
+    epTitleEl.className = 'card-title';
+    epTitleEl.innerText = `${episode.name} S${seasonNr}E${episodeNr}`;
+    cardEl.appendChild(epTitleEl);
+    const pictureEl = document.createElement('img');
+    pictureEl.src = episode.image.medium;
+    pictureEl.className = 'card-img';
+    cardEl.appendChild(pictureEl);
+    const summaryEl = document.createElement('div');
+    summaryEl.innerHTML = episode.summary;
+    summaryEl.className = 'card-summary';
+    cardEl.appendChild(summaryEl);
+    rootEl.appendChild(cardEl);
+  }
 }
