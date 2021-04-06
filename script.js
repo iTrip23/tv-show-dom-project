c7ba6e945ebe310a8c59483004bb2c8fa6e16eab
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-04-06 20:30:43 +0100

    400 done

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
 let allEpisodes;
 let filteredEpisodes;
 let allTVShows;
+let selectedShow;
+
 const rootEl = document.querySelector('#root');
 const searchInput = document.querySelector('#search-input');
 const searchResult = document.querySelector('#search-result');
 searchInput.value = '';
-const selectEl = document.querySelector('#select');
-let selectAll = document.createElement('option');
-selectAll.addEventListener('click', () => {
+const selectEpisodesEl = document.querySelector('#select');
+const TVShowsEl = document.querySelector('#TVShows');
+const optionALL = document.createElement('option');
+
+optionALL.innerText = 'All TV Series';
+optionALL.addEventListener('click', () => {
+  console.log(allTVShows);
   let parentEl = '';
-  allEpisodes.forEach(episode => parentEl += getEpisodeCard(episode));
+  allTVShows.forEach(show => parentEl += getShowCard(show))
   rootEl.innerHTML = parentEl;
-  searchResult.innerHTML = `Displaying ${allEpisodes.length}/${allEpisodes.length}`;
 });
-selectAll.innerText = `All Episodes`;
-selectEl.appendChild(selectAll);
+TVShowsEl.appendChild(optionALL);
 
 const getEpisodeTitle = obj => 'S' + `${obj.season}`.padStart(2, 0) + 'E' + `${obj.number}`.padStart(2, 0) + ` - ${obj.name}`;
-const getEpisodeCard = obj => `<div class="row col-md-3 m-1 card">
-  <div class="card-body">
+const getEpisodeCard = obj => `<div class="col-md-3 m-1 card">
+  <div class="card-body card-css">
     <h5 class="card-title text-center">${getEpisodeTitle(obj)}</h5>
     <hr>
-    <img src="${obj.image.medium}" class="card-img-top" alt="">
+    <img src="${obj.image.medium}" class="card-img-top" alt="${getEpisodeTitle(obj)} image">
     <p class="card-text">${obj.summary}</p>
   </div>
 </div>`;
+const getShowCard = obj => `
+<div class="card col-10 m-3">
+  <div class="row g-0">
+    <div class="col-md-3">
+      <div class="card-body">
+        <img src="${obj.image.medium}" alt="${obj.name} image" class="img-fluid"/>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="card-body">
+        <h4 class="card-title">${obj.name}</h4>
+        <p class="card-text">${obj.summary}</p>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card-body">
+        <p class="card-text">Rating: ${obj.rating.average}</p>
+        <p class="card-text">Genre: ${obj.genres}</p>
+        <p class="card-text">Status: ${obj.status}</p>
+        <p class="card-text">Runtime: ${obj.runtime}</p>
+        <p class="card-text">Runtime: ${obj.officialsite}</p>
+      </div>
+    </div>
+  </div>
+</div>
+`;
 
 function displayEpisodes(arr) {
   let parentEl = '';
@@ -33,40 +63,70 @@ function displayEpisodes(arr) {
   searchResult.innerHTML = `Displaying ${allEpisodes.length}/${allEpisodes.length}`;
 }
 
-function getGoT() {
-  fetch('https://api.tvmaze.com/shows/82/episodes')
-    .then(res => res.json())
-    .then(data => {
-      allEpisodes = data;
-      buildWebPage();
-    })
-}
+// function getGoT() {
+//   fetch('https://api.tvmaze.com/shows/82/episodes')
+//     .then(res => res.json())
+//     .then(data => {
+//       allEpisodes = data;
+//       displayShow();
+//     })
+// }
 
-getGoT();
+// getGoT();
 
 searchInput.addEventListener('keyup', (input) => {
   const searchEl = input.target.value.toLowerCase();
   filteredEpisodes = allEpisodes.filter(episode => episode.name.toLowerCase().includes(searchEl) || episode.summary.toLowerCase().includes(searchEl));
-  searchResult.innerHTML = `Displaying ${filteredEpisodes.length}/${allEpisodes.length} episodes`;
   displayEpisodes(filteredEpisodes);
+  searchResult.innerHTML = `Displaying ${filteredEpisodes.length}/${allEpisodes.length} episodes`;
 });
 
-function buildWebPage() {
+function displayShow() {
+  selectEpisodesEl.innerHTML = '';
+  const selectAll = document.createElement('option');
+  selectAll.addEventListener('click', () => {
+    let parentEl = '';
+    allEpisodes.forEach(episode => parentEl += getEpisodeCard(episode));
+    rootEl.innerHTML = parentEl;
+    searchResult.innerHTML = `Displaying ${allEpisodes.length}/${allEpisodes.length}`;
+  });
+  selectAll.innerText = `All Episodes`;
+  selectEpisodesEl.appendChild(selectAll);
   allEpisodes.forEach(episode => {
     let selectOption = document.createElement('option');
     selectOption.innerText = `${getEpisodeTitle(episode)}`;
-    selectOption.addEventListener('click', () => rootEl.innerHTML = getEpisodeCard(episode), searchResult.innerHTML = `Displaying 1/${allEpisodes.length} episodes`);
-    selectEl.appendChild(selectOption);
+    selectEpisodesEl.appendChild(selectOption);
+    selectOption.addEventListener('click', () => { rootEl.innerHTML = getEpisodeCard(episode); searchResult.innerHTML = `Displaying 1/${allEpisodes.length} episodes` });
   })
   displayEpisodes(allEpisodes);
 }
 
-// function getTVShows() {
-//   fetch('https://api.tvmaze.com/shows')
-//     .then(res => res.json())
-//     .then(data => {
-//       allTVShows = data;
-//       allTVShows.
-//     })
-// }
-// getTVShows();
+function getTVShows() {
+  fetch('https://api.tvmaze.com/shows')
+    .then(res => res.json())
+    .then(data => {
+      allTVShows = data;
+      displayAllShows();
+    })
+}
+
+getTVShows();
+
+function displayAllShows() {
+  let parentEl = '';
+  allTVShows.forEach(show => {
+    parentEl += getShowCard(show);
+    rootEl.innerHTML = parentEl;
+    let optionEl = document.createElement('option');
+    optionEl.innerHTML = show.name;
+    TVShowsEl.appendChild(optionEl);
+    optionEl.addEventListener('click', () => {
+      fetch(`${show._links.self.href}/episodes`)
+        .then(res => res.json())
+        .then(data => {
+          allEpisodes = data;
+          displayShow();
+        })
+    });
+  })
+}
